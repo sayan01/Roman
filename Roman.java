11afc27218eb1445c2becd318a4222d03f85a5ae
Sayan Ghosh <sgic2001@gmail.com>
2019-05-11 19:37:34 +0530

    Added roman to arabic converter

--- Roman.java
+++ Roman.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Roman{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -7,16 +8,45 @@ class Roman{
 			case 1:
 				System.out.print("Enter Roman Number: ");
 				String r = sc.next();
-				System.out.println("Arabic :"+arabic(r));
+				System.out.println("Arabic: "+arabic(r));
 				break;
 			case 2:
 				System.out.print("Enter Arabic Number: ");
 				int n = sc.nextInt();
-				System.out.println("Roman: "+roman(n));
+				if(n<0 || n>=4000){
+					System.err.print("Number cannot be converted to Roman.");
+					System.exit(1);
+				}
+				// System.out.println("Roman: "+roman(n));
 				break;
 			default:
-				System.out.println("Invalid choice.");
+				System.err.println("Invalid choice.");
 				System.exit(1);
 		}
+	}
+	public static int arabic(String roman){
+		int rv = 0;
+		for( int i = roman.length()-1 ; i >= 0 ; i--){
+			int ct = r_d(roman.charAt(i));
+			if(i==roman.length()-1){
+				rv = ct;
+			}
+			else{
+				int nt = r_d(roman.charAt(i+1));
+				if(ct<nt)	rv += -ct;
+				else 		rv += +ct;
+			}
+		}
+		return rv;
+	}
+	
+	private static int r_d(char c){
+		c = Character.toUpperCase(c);
+		if(!"IVXLCDM".contains(c+"")){
+			System.err.println("Invalid Syntax.");
+			System.exit(1);
+		}
+		int[] val = {1,5,10,50,100,500,1000};
+		return val["IVXLCDM".indexOf(c)];
 	}
 }
